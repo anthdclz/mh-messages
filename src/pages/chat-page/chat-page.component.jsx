3fdd6009fb3b46566009571581485072b4c61ae2
Anthony dela Calzada <anthony.delacalzada@gmail.com>
2021-08-07 15:03:12 -0700

    Dedupe messages and deleteMessage

--- src/pages/chat-page/chat-page.component.jsx
+++ src/pages/chat-page/chat-page.component.jsx
@@ -8,14 +8,43 @@ class ChatPage extends React.Component {
     constructor(){
         super();
         this.state = {
-            list: chat
+            chatList: chat
+        }
+    }
+    deleteMessage = (key) => {
+        const newList = this.state.chatList;
+        delete newList[key];
+        this.setState({
+            chatList: newList
+        });
+    }
+    componentDidUpdate(){
+        console.log('update...');
+    }
+    componentDidMount(){
+        // Dedupe Messages
+        const listUniques = new Set();
+        const duplicateKeys = [];
+        let i=0;
+            for (const messageObject of this.state.chatList) {
+            let msg = JSON.stringify({uuid: messageObject.uuid, content: messageObject.content});
+            if(listUniques.has(msg)){
+                duplicateKeys.push(i);
+            }else{
+                listUniques.add(msg);
+            }
+            i++;
+        };
+        duplicateKeys.reverse();
+        for(const key of duplicateKeys){
+            this.deleteMessage(key);
         }
     }
     render(){
-        const { list } = this.state;
+        const { chatList } = this.state;
         return(
             <div id="chat-page">
-                {list.map(({...message}, key) => (
+                {chatList.map(({...message}, key) => (
                     <MessageComponent key={key} index={key} {...message} />
                 ))}
             </div>
